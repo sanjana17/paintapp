89336b12c883639c09e03f6046780936a4657ac5
sanjana17 <navyasanjana4@gmail.com>
2016-10-19 23:33:24 -0400

    added images

--- src/js/paint.js
+++ src/js/paint.js
@@ -2,12 +2,19 @@ var module = angular.module('app.paint.controller',[]);
 
 	module.$inject = ['$scope'];
 	module.controller('paintController',function($scope){
-		$scope.showFlip = false;
-		$scope.flip = function(){
+		
+		$scope.flip = function(img){
+			debugger;
 			var time = new Date();
 			$scope.time = time;
-			$scope.showFlip = true;	
+			$scope.showFlip[img.id] = true;	
 		}
+		$scope.images =[
+			{"id" : 'src/resources/1.jpg',"index": 0},
+			{"id" : 'src/resources/2.jpg',"index" : 1},
+			{"id" : 'src/resources/3.jpg',"index" :2},
+			{"id" : 'src/resources/4.jpg', "index" : 3},
+		]
 		$scope.flipOnce = function(){
 			$scope.showFlip = false;	
 		}
